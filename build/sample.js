d90da647b90bfdd0dff3a528b96416b1eda88e1d
Rameez khan <sardarrameez5555@gmail.com>
2020-09-28 16:20:42 +0500

    TypeScript Practice

---
+++ build/sample.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("Hello World");
+console.log("abcded");
+// let b = 12;
+// let z:number = 45;
+document.write("Hello World from sample.ts");
+function A(b) {
+    return b / 2;
+}
+var z = A(4);
+var x = 5;
+x = "any type";
+var firstName = "Rameez";
+var age = 22;
+var person = firstName + age;
---
+++ src/sample.ts
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+console.log("abcded");
+
+// let b = 12;
+// let z:number = 45;
+document.write("Hello World from sample.ts")
+
+function A(b:number){
+    return b/2;
+}
+let z=A(4);
+
+let x:any=5;
+x="any type";
+
+let firstName: string = "Rameez";
+let age: number = 22;
+let person: any= firstName+age;
